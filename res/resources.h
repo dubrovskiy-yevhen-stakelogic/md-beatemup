07f68924607ea49ee00c823f83ab3e054c717ffd
Yevhen Dubrovskyi <jknoxville712@gmail.com>
2026-06-01 01:21:06 +0300

    added sprite animations (idle/waking)

---
+++ res/resources.h
@@ -0,0 +1,8 @@
+#include <genesis.h>
+
+#ifndef _RES_RESOURCES_H_
+#define _RES_RESOURCES_H_
+
+extern const SpriteDefinition player_sprite;
+
+#endif // _RES_RESOURCES_H_
---
+++ res/resources.res
@@ -0,0 +1,1 @@
+SPRITE player_sprite "player_recut.png" 13 16 FAST 4
